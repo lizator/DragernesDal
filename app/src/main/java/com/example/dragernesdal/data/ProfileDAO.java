9bd628964521b9f3fe3edfc9b9d6e9ba9da1d3ff
lizator <frederikkoefoeds@gmail.com>
2020-10-24 22:51:05 +0200

    Added create user call and fixed error with only seeting email to "test@gmail.com"

--- app/src/main/java/com/example/dragernesdal/data/ProfileDAO.java
+++ app/src/main/java/com/example/dragernesdal/data/ProfileDAO.java
@@ -1,16 +1,12 @@
 package com.example.dragernesdal.data;
 
 
-import android.widget.Toast;
-
 import com.example.dragernesdal.data.model.ProfileDTO;
-import com.example.dragernesdal.ui.login.LoginActivity;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -18,19 +14,31 @@ import retrofit2.http.*;
 
 public class ProfileDAO {
 
+    private Retrofit retrofit;
+    private profileCallService service;
+
     Response<ProfileDTO> resp;
 
-    ProfileDTO getProfileByEmail(String email) throws IOException {
-        Retrofit retrofit = new Retrofit.Builder()
+    public ProfileDAO() {
+        this.retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("http://10.16.162.242:8080/")
                 .build();
+        this.service = retrofit.create(profileCallService.class);
+    }
 
-        CallService service = retrofit.create(CallService.class);
+    ProfileDTO getProfileByEmail(String email) throws IOException {
 
         ProfileDTO dto = new ProfileDTO();
-        dto.setEmail("test@gmail.com");
+        dto.setEmail(email);
         Call<ProfileDTO> call = service.getByEmail(dto);
+        resp = call.execute();
+        return resp.body();
+    }
+
+    ProfileDTO createUser(ProfileDTO dto) throws IOException {
+
+        Call<ProfileDTO> call = service.createUser(dto);
         resp = call.execute();
         return resp.body();
     }
@@ -40,9 +48,12 @@ public class ProfileDAO {
         return;
     }
 
-    public interface CallService {
+    public interface profileCallService {
         @POST("user/getbyemail")
         Call<ProfileDTO> getByEmail(@Body ProfileDTO dto);
+
+        @POST("user/create")
+        Call<ProfileDTO> createUser(@Body ProfileDTO dto);
     }
 }
 
