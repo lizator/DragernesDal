6d57d33f0fefbb3fb18fa9a666eae81a936cc7e3
lizator <frederikkoefoeds@gmail.com>
2020-11-05 16:14:53 +0100

    updating loginHandler to save in shared pref for main to get

--- app/src/main/java/com/example/dragernesdal/ui/login/LoginHandler.java
+++ app/src/main/java/com/example/dragernesdal/ui/login/LoginHandler.java
@@ -9,33 +9,39 @@ import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 public class LoginHandler { // for saved login
     static final String PREF_USER_EMAIL= "userEmail";
     static final String PREF_PASS_HASH= "userPassHash";
+    static final String PREF_DISPLAY_NAME= "userDisplayName";
     public static final int LOGOUT_CODE = 1;
-    private Context ctx;
+    private SharedPreferences prefs;
 
     public LoginHandler(Context ctx) {
-        this.ctx = ctx;
+        prefs = ctx.getSharedPreferences("login_pref", ctx.MODE_MULTI_PROCESS);
     }
 
 
-    public void setUser(String email, String passHash) {
-        SharedPreferences.Editor editor = getDefaultSharedPreferences(ctx).edit();
+    public void setUser(String email, String passHash, String displayName) {
+        SharedPreferences.Editor editor = prefs.edit();
         editor.putString(PREF_USER_EMAIL, email);
         editor.putString(PREF_PASS_HASH, passHash);
+        editor.putString(PREF_DISPLAY_NAME, displayName);
         editor.commit();
     }
 
     public String getEmail() {
-        return getDefaultSharedPreferences(this.ctx).getString(PREF_USER_EMAIL, "");
+        return prefs.getString(PREF_USER_EMAIL, "");
     }
 
     public String getPasshash() {
-        return getDefaultSharedPreferences(this.ctx).getString(PREF_PASS_HASH, "");
+        return prefs.getString(PREF_PASS_HASH, "");
+    }
+
+    public String getDisplayName() {
+        return prefs.getString(PREF_DISPLAY_NAME, "");
     }
 
 
     public void clear()
     {
-        SharedPreferences.Editor editor = getDefaultSharedPreferences(this.ctx).edit();
+        SharedPreferences.Editor editor = prefs.edit();
         editor.clear(); //clear all stored data
         editor.commit();
     }
