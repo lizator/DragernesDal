1e4da336ba724881bf0320dc12fa6aa2c629d266
Mark <s174881@student.dtu.dk>
2021-01-04 12:22:02 +0100

    Recylcer view med cardviews til event

--- app/src/main/java/com/rbyte/dragernesdal/ui/event/EventFragment.java
+++ app/src/main/java/com/rbyte/dragernesdal/ui/event/EventFragment.java
@@ -1,40 +1,55 @@
 package com.rbyte.dragernesdal.ui.event;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.rbyte.dragernesdal.R;
+import com.rbyte.dragernesdal.ui.character.create.ChooseRaceFragment;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class EventFragment extends Fragment {
 
     private EventViewModel eventViewModel;
+    private EventAdapter eventAdapter = new EventAdapter();
     private CalendarView calendar;
     private Button button_event_info;
+    private ArrayList<EventCard> eventCards = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         eventViewModel =
                 new ViewModelProvider(this).get(EventViewModel.class);
         View root = inflater.inflate(R.layout.fragment_event, container, false);
+        eventCards.add(new EventCard("Nu","Det gamle hus ved stranden"));
+        eventCards.add(new EventCard("I morgen","Det nye hus ved stranden"));
 
-
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.eventRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setAdapter(eventAdapter);
+        eventAdapter.notifyDataSetChanged();
 
         OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
             @Override
@@ -48,8 +63,66 @@ public class EventFragment extends Fragment {
         return root;
     }
 
+    class EventViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+        TextView date;
+        TextView info;
+        public EventViewHolder(View eventViews) {
+            super(eventViews);
+            cardView = eventViews.findViewById(R.id.event_card_view);
+            date = eventViews.findViewById(R.id.textDate);
+            info = eventViews.findViewById(R.id.textEventInfo);
 
+            // Gør listeelementer klikbare og vis det ved at deres baggrunsfarve ændrer sig ved berøring
+            cardView.setBackgroundResource(android.R.drawable.list_selector_background);
+        }
+    }
 
+    class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
+        @Override
+        public int getItemCount() {
+            return eventCards.size();
+        }
 
+        @Override
+        public EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View listElementViews = getLayoutInflater().inflate(R.layout.recycler_event_cardview, parent, false);
+            EventViewHolder vh = new EventViewHolder(listElementViews);
+            return vh;
+        }
 
+        @Override
+        public void onBindViewHolder(EventViewHolder vh, int position) {
+            vh.date.setText(eventCards.get(position).getDate());
+            vh.info.setText(eventCards.get(position).getInfo());
+        }
+
+    }
+
+    private class EventCard {
+        private String date;
+        private String info;
+
+        public EventCard(String date, String info) {
+            this.date = date;
+            this.info = info;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+
+        public void setInfo(String info) {
+            this.info = info;
+        }
+
+    }
 }
