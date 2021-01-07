5af6e12c219c4cad94b5286b34cc3fa81eddea35
Mark <s174881@student.dtu.dk>
2021-01-07 13:53:47 +0100

    Opret event fragment

--- app/src/main/java/com/rbyte/dragernesdal/ui/admin/AdminFragment.java
+++ app/src/main/java/com/rbyte/dragernesdal/ui/admin/AdminFragment.java
@@ -6,14 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
---
+++ app/src/main/java/com/rbyte/dragernesdal/ui/admin/event/CreateEventFragment.java
@@ -0,0 +1,4 @@
+package com.rbyte.dragernesdal.ui.admin.event;
+//https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
+public class CreateEventFragment {
+}
