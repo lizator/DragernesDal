d09ef5caaae523e74c1302e75f8324f8770aa811
Lizator <frederikkoefoeds@gmail.com>
2021-01-14 13:24:27 +0100

    design and btns fully implemented

--- app/src/main/java/com/rbyte/dragernesdal/ui/character/inventory/InventoryFragment.java
+++ app/src/main/java/com/rbyte/dragernesdal/ui/character/inventory/InventoryFragment.java
@@ -1,13 +1,19 @@
 package com.rbyte.dragernesdal.ui.character.inventory;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -24,6 +30,8 @@ import com.rbyte.dragernesdal.data.inventory.model.InventoryDTO;
 import com.rbyte.dragernesdal.ui.PopupHandler;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class InventoryFragment extends Fragment {
 
@@ -33,8 +41,13 @@ public class InventoryFragment extends Fragment {
     private InventoryAdapter adapter = new InventoryAdapter();
     private PopupHandler popHandler;
     private View root2;
+    private Handler uithread = new Handler();
     private ArrayList<InventoryDTO> items = new ArrayList<>();
     private RecyclerView recyclerView;
+    private LinearLayoutManager llm;
+    private EditText goldEdit;
+    private EditText silverEdit;
+    private EditText copperEdit;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,24 +59,72 @@ public class InventoryFragment extends Fragment {
         InventoryDTO gold = inventory.get(0);
         InventoryDTO silver = inventory.get(1);
         InventoryDTO copper = inventory.get(2);
+        goldEdit = root.findViewById(R.id.goldEdit);
+        silverEdit = root.findViewById(R.id.silverEdit);
+        copperEdit = root.findViewById(R.id.copperEdit);
+
+        goldEdit.setText(gold.getAmount() +"");
+        silverEdit.setText(silver.getAmount() +"");
+        copperEdit.setText(copper.getAmount() +"");
 
         if (inventory.size() > 3){
             items.clear();
             for (int i = 3; i < inventory.size(); i++){
                 items.add(inventory.get(i));
             }
+        } else {
+            items.clear();
+            InventoryDTO dto = new InventoryDTO();
+            dto.setIdItem(3);
+            dto.setIdInventoryRelation(inventoryRepo.getRelationID());
+            dto.setItemName("Indsæt Genstand");
+            dto.setAmount(0);
+            items.add(dto);
         }
 
-
-        // adding last line with btn to recycler
-        InventoryDTO dto = new InventoryDTO();
-        dto.setIdItem(-1);
-        items.add(dto);
-
         recyclerView = (RecyclerView) root.findViewById(R.id.inventoryRecycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        llm = new LinearLayoutManager(root.getContext());
+        recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(adapter);
 
+        ((Button) root.findViewById(R.id.newLinebtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InventoryDTO dto = new InventoryDTO();
+                dto.setIdItem(items.get(items.size()-1).getIdItem()+1);
+                dto.setIdInventoryRelation(inventoryRepo.getRelationID());
+                dto.setItemName("");
+                dto.setAmount(0);
+                items.add(dto);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        ((Button) root.findViewById(R.id.savebtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Executor bgThread = Executors.newSingleThreadExecutor();
+                bgThread.execute(() -> {
+                    ArrayList<InventoryDTO> newInventory = new ArrayList<>();
+                    gold.setAmount(Integer.parseInt(goldEdit.getText().toString()));
+                    silver.setAmount(Integer.parseInt(silverEdit.getText().toString()));
+                    copper.setAmount(Integer.parseInt(copperEdit.getText().toString()));
+                    newInventory.add(gold);
+                    newInventory.add(silver);
+                    newInventory.add(copper);
+                    newInventory.addAll(items);
+
+                    inventoryRepo.saveInventory(charRepo.getCurrentChar().getIdcharacter(), newInventory);
+                    inventoryRepo.startGetThread();
+
+                    uithread.post(() -> {
+                       root2.findViewById(R.id.updateTV).setVisibility(View.VISIBLE);
+                        Toast.makeText(getContext(), "testing", Toast.LENGTH_SHORT);
+                    });
+
+                });
+            }
+        });
 
 
 
@@ -84,22 +145,15 @@ public class InventoryFragment extends Fragment {
     class InventoryViewHolder extends RecyclerView.ViewHolder{
         EditText name;
         EditText amount;
-        LinearLayout lineView;
-        Button addbtn;
-        LinearLayout btnView;
+        ImageView closeImg;
         View view;
         public InventoryViewHolder(View abilityViews) {
             super(abilityViews);
             view = abilityViews;
             name = abilityViews.findViewById(R.id.itemNameEdit);
             amount = abilityViews.findViewById(R.id.itemAmountEdit);
-            lineView = abilityViews.findViewById(R.id.itemLine);
-            addbtn = abilityViews.findViewById(R.id.newLineBtn);
-            btnView = abilityViews.findViewById(R.id.newItemLine);
+            closeImg = abilityViews.findViewById(R.id.closeLine);
             // Gør listeelementer klikbare og vis det ved at deres baggrunsfarve ændrer sig ved berøring
-            name.setBackgroundResource(android.R.drawable.list_selector_background);
-            amount.setBackgroundResource(android.R.drawable.list_selector_background);
-            addbtn.setBackgroundResource(android.R.drawable.list_selector_background);
         }
 
     }
@@ -120,33 +174,55 @@ public class InventoryFragment extends Fragment {
         @Override
         public void onBindViewHolder(InventoryViewHolder vh, int position) {
             if (position % 2 == 1) vh.view.setBackgroundColor(getResources().getColor(R.color.colorTableLine1));
-            if (items.get(position).getIdItem() != -1) {
-                vh.name.setText(items.get(position).getItemName());
-                vh.amount.setText(items.get(position).getAmount() + "");
-            } else {
-                vh.lineView.setVisibility(View.GONE);
-                vh.btnView.setVisibility(View.VISIBLE);
-                vh.btnView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
 
-                        InventoryDTO dto = new InventoryDTO();
-                        if (items.size() == 1) {
-                            dto.setIdItem(3);
-                            dto.setIdInventoryRelation(inventoryRepo.getRelationID());
+            TextWatcher lineChangeWatcher = new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    // ignore
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    // ignore
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    for (int i = 0; i < items.size(); i++){
+                        View view = llm.findViewByPosition(i);
+                        if (view != null) {
+                            String itemName = ((EditText) view.findViewById(R.id.itemNameEdit)).getText().toString();
+                            String number = ((EditText) view.findViewById(R.id.itemAmountEdit)).getText().toString();
+                            items.get(i).setItemName(itemName);
+                            try {
+                                int amount = Integer.parseInt(number);
+                                items.get(i).setAmount(amount);
+                            } catch (NumberFormatException e){
+                                Log.d("InventoryFragment", "lineChangeWatcher: amount not found error");
+                            }
+
                         }
-                        else {
-                            dto.setIdItem(items.get(items.size()-2).getIdItem() + 1);
-                            dto.setIdInventoryRelation(items.get(items.size()-2).getIdInventoryRelation());
-                        }
-                        dto.setItemName("Indsæt Genstand");
-                        dto.setAmount(0);
-                        items.add(items.size()-1, dto);
-                        adapter.notifyDataSetChanged();
                     }
-                });
-            }
+                }
+            };
 
+            vh.name.setText(items.get(position).getItemName());
+            vh.amount.setText(items.get(position).getAmount() + "");
+            vh.name.addTextChangedListener(lineChangeWatcher);
+            vh.amount.addTextChangedListener(lineChangeWatcher);
+
+            vh.closeImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    popHandler.getConfirmRemoveInventoryAlert(root2, items.get(position).getItemName(), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            items.remove(position);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }).show();
+                }
+            });
         }
     }
 }
